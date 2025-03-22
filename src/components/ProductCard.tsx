6bb70961ab0c416db76097ab03be60665af23200
sondosomarr <sondosomarr2020@gmail.com>
2025-03-22 02:59:17 +0200

    Refactor product listing to use ProductCard component and implement API service for fetching products

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,31 @@
+import ProductType from '@/types/productType'
+import React from 'react'
+interface Props{
+    products:ProductType
+}
+export default function ProductCard({products}:Props) {
+  return (
+    <>
+{/* Product Card */}
+<div
+    
+    className="bg-white rounded-xl shadow-lg p-4 hover:shadow-xl transition duration-300"
+  >
+    <img
+      src={products.image}
+      alt={products.title}
+      className="w-full h-48 object-contain mb-4"
+    />
+    <h2 className="text-sm font-semibold">{products.title}</h2>
+    <p className="text-gray-600 text-sm mb-2">{products.category}</p>
+    <p className="text-green-600 font-bold">${products.price}</p>
+    <button className="mt-3 w-full bg-orange-700 text-white py-2 rounded-md hover:bg-orange-800 transition">
+      Add to Cart
+    </button>
+  </div>
+
+    </>
+    
+   
+  )
+}
---
+++ src/types/productType.tsx
@@ -0,0 +1,9 @@
+export default interface ProductType{
+    id: number,
+    title: string,
+    image: string,
+    category: string,
+    // color: string,
+    description: string,
+    price: number,
+  }
